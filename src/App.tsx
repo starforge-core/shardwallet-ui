2693cf8a60f81f531ea2a1661132df43fc341aef
William Chargin <wchargin@gmail.com>
2022-10-02 07:13:39 -0700

    css: remove most Vite default styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-
 import { useWeb3 } from "./lib/web3";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { account, provider, connectMetamask } = useWeb3();
   return (
-    <div className="App">
+    <div className="flex flex-col gap-3">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Account:{" "}
+        {account ?? <button onClick={() => connectMetamask()}>Connect</button>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <HelloWeb3 />
-    </div>
-  );
-}
-
-function HelloWeb3() {
-  const web3 = useWeb3();
-  console.log("rendering HelloWeb3:", web3);
-  return (
-    <div>
-      <button onClick={() => web3.connectMetamask()}>connect</button>
-      <br />
-      you are {web3.account ?? "not connected"}
     </div>
   );
 }
