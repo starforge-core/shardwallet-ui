9a959adf3da6c294cbe3d48ca48fcbb63e806ac3
William Chargin <wchargin@gmail.com>
2022-10-04 15:50:57 -0700

    Clarify "will claim" dollar-equivalent wording

--- src/App.tsx
+++ src/App.tsx
@@ -265,7 +265,7 @@ function App() {
               {ethPrice != null && (
                 <>
                   {" "}
-                  (
+                  (will claim{" "}
                   {ethToDisplayUsd(
                     Object.entries(sw.balances)
                       .reduce(
